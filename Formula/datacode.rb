7a82486463a34012c423769138f0d3afa22a57c5
sagiller <sagiller@minipc.sagiller.com>
2026-08-30 23:35:29 +0800

    feat: publish DataCode 1.0.7

--- Formula/datacode.rb
+++ Formula/datacode.rb
@@ -6,27 +6,27 @@ class Datacode < Formula
   desc "AI coding agent for the terminal"
   homepage "https://github.com/sagiller/datacode-releases"
   license "MIT"
-  version "1.0.6"
+  version "1.0.7"
 
   depends_on "ripgrep"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.6/datacode-darwin-arm64.zip"
-      sha256 "6b0196df034629a9eb1b2f8a282d1c110a23febca3117f7c75271e979fb05863"
+      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.7/datacode-darwin-arm64.zip"
+      sha256 "ccf70d779a9a092fdf8eb79512e2fe784d59c855cb66cb9e5fcec4a534181512"
     else
-      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.6/datacode-darwin-x64.zip"
-      sha256 "3f790749938f2aefac742360137b17a91375c6bebca4742218054f0fec538a5c"
+      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.7/datacode-darwin-x64.zip"
+      sha256 "a7d53b424fca8fa1bc66c370a14c5ed60e9551c0d53fd2981dbcd19f62f14926"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.6/datacode-linux-arm64.tar.gz"
-      sha256 "08c10c6866e1637c10e5de7b4f7b348ea73bfb033897a551a2972437450bab7d"
+      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.7/datacode-linux-arm64.tar.gz"
+      sha256 "ca85c8a4e25e9c184931310e0386950e257157f12cea516e33602196ee7b19d6"
     else
-      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.6/datacode-linux-x64.tar.gz"
-      sha256 "0513848e9010eaed090f616c5e5fde7cd1fd183ee305552c351c26a649c82fff"
+      url "https://github.com/sagiller/datacode-releases/releases/download/v1.0.7/datacode-linux-x64.tar.gz"
+      sha256 "c4519f8c32c9113a49b1c6009efe4c39d91677c4ca1ee2baaa7f787766d1f5fd"
     end
   end
 
